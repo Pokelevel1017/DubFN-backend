516d9eef2ca95e0e58270c81997ffd4e04d3a1a6
Pokelevel1017 <104866158+Pokelevel1017@users.noreply.github.com>
2022-05-12 14:29:01 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    host: 'localhost',
+    port: 8080,
+    directory: __dirname,
+    dir: __dirname,
+    season_num: 19, // Update this whenever there is a new season.
+    banner: 'https://raw.githubusercontent.com/FBK45/Carbon/raw/main/uqujxr9mju581.jpg',
+
+    background: 'worldcup'
+    /*
+    worldcup - WorldCup
+    winter19 - idk
+    GalileoLite - Star Wars Lite
+    halloween2020 - Halloween 
+    season15xmas - Cool XMas Background
+    fortnitemares - Somehow Winterfest 2019 
+    BlackMonday - Batman 
+    Galileo - Star Wars
+    season16 - Primal Season
+    season15 - Shit season
+    season15xmas - Winterfest 2020
+    season14 - Marvel
+    season13 - Water season
+    season12 - Spy season 
+    season11 - Chapter 2
+    seasonx - Season 10
+    */
+}
